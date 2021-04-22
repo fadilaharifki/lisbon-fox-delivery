221406e20a38fc41e553d3c1a129d3c11feaa4fe
farel14 <72905415+farel14@users.noreply.github.com>
2021-04-23 04:57:58 +0700

    datalist Added

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+// Function
 function tambahKota(asal, tujuan, tarif) {
+    asal = capitalizeWord(asal)
+    tujuan = capitalizeWord(tujuan)
+
     const result = database.push({ city: [asal, tujuan].sort(), cost: tarif })
+
+    for (let i = 0; i <= cityList.length; i++) {
+        // console.log(cityList[i]);
+        if (cityList[i] === asal) {
+            break
+        } else if (i === cityList.length) {
+            cityList.push(asal)
+            break
+        }
+    }
+    for (let i = 0; i <= cityList.length; i++) {
+        // console.log(cityList[i]);
+        if (cityList[i] === tujuan) {
+            break
+        } else if (i === cityList.length) {
+            cityList.push(tujuan)
+            break
+        }
+    }
+
     return result
 }
 
 function hapusKota(asal, tujuan) {
+    asal = capitalizeWord(asal)
+    tujuan = capitalizeWord(tujuan)
     let array = [asal, tujuan].sort()
 
     for (let i = 0; i < database.length; i++) {
@@ -11,9 +37,12 @@ function hapusKota(asal, tujuan) {
             database.splice(i, 1)
         }
     }
+
 }
 
 function cekTarif(asal, tujuan, berat) {
+    asal = capitalizeWord(asal)
+    tujuan = capitalizeWord(tujuan)
     const city = [asal, tujuan].sort()
     let tarif = 0
 
@@ -28,6 +57,9 @@ function cekTarif(asal, tujuan, berat) {
 }
 
 function tambahOrder(asal, tujuan, berat, deskripsi) {
+    asal = capitalizeWord(asal)
+    tujuan = capitalizeWord(tujuan)
+    deskripsi = capitalizeWord(deskripsi)
     let result = {
         id: 1,
         origin: asal,
@@ -48,12 +80,12 @@ function tambahOrder(asal, tujuan, berat, deskripsi) {
 function lacakOrder(id) {
     let result = {}
     for (let i = 0; i <= order.length; i++) {
-        if (!order[i]){
-            result='paket tidak ditemukan'
+        if (!order[i]) {
+            result = 'paket tidak ditemukan'
             break
-        }   
+        }
 
-        if (id === order[i].id){
+        if (id === order[i].id) {
             result = order[i]
             break
         }
@@ -61,17 +93,35 @@ function lacakOrder(id) {
     return result
 }
 
+function generateCity(data) {
+    const list = document.getElementById('browsers');
+
+    for (let i = 0; i < data.length; i++) {
+        const option = document.createElement('option')
+        option.value = data[i]
+        list.appendChild(option);
+        // console.log(data[i])
+    }
+}
+
+function capitalizeWord(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// Data
 
 const database = [
-    { city: ['bandung', 'jakarta'], cost: '9000' },
-    { city: ['bandung', 'semarang'], cost: '22000' },
-    { city: ['bandung', 'surabaya'], cost: '12500' },
-    { city: ['jakarta', 'semarang'], cost: '17500' },
-    { city: ['jakarta', 'surabaya'], cost: '11000' },
-    { city: ['semarang', 'surabaya'], cost: '19000' },
+    { city: ['Bandung', 'Jakarta'], cost: '9000' },
+    { city: ['Bandung', 'Semarang'], cost: '22000' },
+    { city: ['Bandung', 'Surabaya'], cost: '12500' },
+    { city: ['Jakarta', 'Semarang'], cost: '17500' },
+    { city: ['Jakarta', 'Surabaya'], cost: '11000' },
+    { city: ['Semarang', 'Surabaya'], cost: '19000' },
 ]
 
 const order = []
+
+const cityList = ['Bandung', 'Jakarta', 'Semarang', 'Surabaya']
 
 // DOM
 /* const addCity = document.getElementById('addCity')
@@ -88,22 +138,27 @@ trackOrder.addEventListener("click", lacakOrder) */
 
 
 // TestCode
-// tambahKota('bekasi', 'jakarta', '12000')
-// tambahKota('jakarta', 'tangerang', '14000')
+// tambahKota('bekasi', 'Jakarta', '12000')
+// tambahKota('Jakarta', 'tangerang', '14000')
 
-// hapusKota('jakarta','bekasi')
-// console.log(cekTarif('jakarta', 'bandung', 5))
-// console.log(database) 
+// console.log(cityList)
+// hapusKota('Jakarta','bekasi')
+// console.log(cekTarif('Jakarta', 'Bandung', 5))
+// console.log(database)
 
-// tambahOrder('jakarta', 'bandung', 5, 'mainan')
-// tambahOrder('semarang', 'bandung', 2, 'makanan')
-// tambahOrder('jakarta', 'surabaya', 3, 'mobil')
+// tambahOrder('Jakarta', 'Bandung', 5, 'mainan')
+// tambahOrder('Semarang', 'Bandung', 2, 'makanan')
+// tambahOrder('Jakarta', 'Surabaya', 3, 'mobil')
 // console.log(order)
 // console.log(lacakOrder(7))
 
 
 /* [
-    { city: [ 'bekasi', 'jakarta' ], cost: '15000' },
-    { city: [ 'jakarta', 'tangerang' ], cost: '14000' },
-    { city: [ 'bandung', 'jakarta' ], cost: '15000' }
+    { city: [ 'bekasi', 'Jakarta' ], cost: '15000' },
+    { city: [ 'Jakarta', 'tangerang' ], cost: '14000' },
+    { city: [ 'Bandung', 'Jakarta' ], cost: '15000' }
   ] */
+
+
+// Invoke function
+// generateCity(cityList)
