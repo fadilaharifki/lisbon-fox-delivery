2f3866d36d174faeae43e429e28a4953e5ba3aa7
farel14 <72905415+farel14@users.noreply.github.com>
2021-04-22 16:35:41 +0700

    hapusKota+beratCekTarif

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 function tambahKota(asal, tujuan, tarif) {
-    let result = database.push({ kota: [asal, tujuan].sort(), cost: tarif })
+    const result = database.push({ city: [asal, tujuan].sort(), cost: tarif })
     return result
 }
 
-function cekTarif(asal, tujuan, data) {
-    const kota = [asal, tujuan].sort()
+function hapusKota(asal, tujuan) {
+    // delete database[]
+    let array = [asal, tujuan].sort()
+
+    for (let i = 0; i < database.length; i++) {
+        if (array[0] === database[i].city[0] && array[1] === database[i].city[1]) {
+            database.splice(i, 1)
+        }
+    }
+}
+
+function cekTarif(asal, tujuan, berat) {
+    const city = [asal, tujuan].sort()
     let tarif = 0
 
-    for (let j = 0; j < data.length; j++) {
-        if (kota[0] === data[j].kota[0] && kota[1] === data[j].kota[1]) {
-            tarif = data[j].cost
+    for (let j = 0; j < database.length; j++) {
+        if (city[0] === database[j].city[0] && city[1] === database[j].city[1]) {
+            tarif = database[j].cost
             break
         }
     }
+    tarif *= berat
     return tarif
 }
 
 
 const database = [
-    { kota: ['semarang', 'surabaya'], cost: '15000' }
+    { city: ['bandung', 'jakarta'], cost: '9000' },
+    { city: ['bandung', 'semarang'], cost: '22000' },
+    { city: ['bandung', 'surabaya'], cost: '12500' },
+    { city: ['jakarta', 'semarang'], cost: '17500' },
+    { city: ['jakarta', 'surabaya'], cost: '11000' },
+    { city: ['semarang', 'surabaya'], cost: '19000' },
 ]
 
 
@@ -27,15 +44,17 @@ const submitButton = document.getElementById('submit')
 submitButton.addEventListener("click", tambahKota) */
 
 
+/* // TestCode
 tambahKota('bekasi', 'jakarta', '12000')
 tambahKota('jakarta', 'tangerang', '14000')
-tambahKota('jakarta', 'bandung', '15000')
-// console.log(cekTarif('jakarta', 'bekasi', database))
-// console.log(database)
+
+hapusKota('jakarta','bekasi')
+console.log(cekTarif('jakarta', 'bandung', 5))
+console.log(database) */
 
 
 /* [
-    { kota: [ 'bekasi', 'jakarta' ], cost: '15000' },
-    { kota: [ 'jakarta', 'tangerang' ], cost: '14000' },
-    { kota: [ 'bandung', 'jakarta' ], cost: '15000' }
+    { city: [ 'bekasi', 'jakarta' ], cost: '15000' },
+    { city: [ 'jakarta', 'tangerang' ], cost: '14000' },
+    { city: [ 'bandung', 'jakarta' ], cost: '15000' }
   ] */
